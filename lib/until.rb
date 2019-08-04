5453945a206484c53f4388653edd89ad865c8876
Rida Kodad <kodadreda@gmail.com>
2019-08-04 16:37:15 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
       until  levitation_force < 10
-        puts "levitation_force is less than 10"
+        puts "Wingardium Leviosa"
         levitation_force += 1
       end
 end
