31046d8bafabe3af229630c33357e0efe1aa914e
Rida Kodad <kodadreda@gmail.com>
2019-08-04 16:35:39 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,8 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
+      until  levitation_force < 10 
+        puts "levitation_force is less than 10"
+        levitation_force -= 1
+      end
 end
 
